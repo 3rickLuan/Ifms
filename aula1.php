6d4500c47793a35bc0a354150b4f1f9643edf9e8
Erick <61208942+3rickLuan@users.noreply.github.com>
2022-08-19 18:26:31 -0400

    Add files via upload

---
+++ aula1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Aula</title>
+</head>
+<body>
+    <p>Boa noite</p>
+    <a href="#">Não Faz nada</a>
+    <table>
+        <tr>
+            <td>aaaa</td>
+            <td>bbbb</td>
+            <td>cccc</td>
+        </tr>
+        <tr>
+            <td>dddd</td>
+            <td>eeee</td>
+            <td>ffff</td>
+        </tr>
+        <tr>
+            <td>gggg</td>
+            <td>hhhh</td>
+            <td>iiii</td>
+        </tr>
+    </table>
+</body>
+</html>
